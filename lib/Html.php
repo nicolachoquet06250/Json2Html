0368b60903772ee84675e1a3d17ababbf0c4bae2
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-04-25 08:39:02 +0200

    ajout de la gestion des balises HTML

---
+++ lib/Html.php
@@ -0,0 +1,20 @@
+<?php
+
+class Html {
+	const COMMENT 		= "";
+	const DOCTYPE 		= "Doctype";
+	const A 			= "a";
+	const ABBR 			= "abbr";
+	const ADDRESS 		= "address";
+	const AREA 			= "area";
+	const ARTICLE 		= "article";
+	const ASIDE 		= "aside";
+	const AUDIO 		= "audio";
+	const B 			= "b";
+	const BASE 			= "base";
+	const BDO 			= "bdo";
+	const BLOCKQUOTE 	= "blockquote";
+	const BODY 			= "body";
+	const BR 			= "br";
+	const HEAD 			= "head";
+}
